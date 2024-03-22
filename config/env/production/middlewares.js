b19d72f096cac73ce25d52719754c3d86d960dde
WILLIAM ELLIS <will@wearestudio77.com>
2024-03-22 16:31:10 -0600

    potential middlewares fix to csp policy

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,22 +1,21 @@
 module.exports = ({ env }) => [
+  'strapi::errors',
   {
     name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         directives: {
           'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'https://market-assets.strapi.io',
-            'cdn.jsdelivr.net',
-            'strapi.io',
-            'dl.airtable.com',
-            'kinsman.sfo2.digitaloceanspaces.com',
-          ],
+          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', 'kinsman.sfo2.digitaloceanspaces.com'],
         },
-      },
+      }
     },
   },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::favicon',
+  'strapi::public',
 ];
