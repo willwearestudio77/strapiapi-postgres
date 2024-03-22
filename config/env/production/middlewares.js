513580c68d74b71e0e57e6aba20a83093c7b25fd
WILLIAM ELLIS <will@wearestudio77.com>
2024-03-22 15:52:44 -0600

    middleware fix for cors issue

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,12 +1,21 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'cdn.jsdelivr.net',
+            'strapi.io',
+            'dl.airtable.com',
+            'kinsman.sfo2.digitaloceanspaces.com',
+          ],
+        },
+      },
+    },
+  },
+
 ];
