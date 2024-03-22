1976cf9501657ad40a6e8fbd2e3792249220e6ea
WILLIAM ELLIS <will@wearestudio77.com>
2024-03-22 16:43:26 -0600

    added more to csp

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,21 +1,40 @@
-module.exports = ({ env }) => [
+export default [
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        directives: {
-          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
-          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', 'kinsman.sfo2.digitaloceanspaces.com'],
-        },
-      }
-    },
-  },
+  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
   'strapi::body',
+  'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'", 
+            "data:", 
+            "blob:", 
+            `https://kinsman.sfo2.digitaloceanspaces.com`, 
+            "dl.airtable.com", 
+            "market-assets.strapi.io"
+          ],
+            "media-src": [
+            "'self'", 
+            "data:", 
+            "blob:", 
+            `https://kinsman.sfo2.digitaloceanspaces.com`, 
+            "dl.airtable.com", 
+            "market-assets.strapi.io"
+        ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,30 +1,14 @@
 module.exports = ({ env }) => ({
-    // ...
     upload: {
       config: {
-        provider: 'aws-s3',
+        provider: "strapi-provider-upload-do",
         providerOptions: {
-          baseUrl: env('CDN_URL'),
-          rootPath: env('CDN_ROOT_PATH'),
-          s3Options: {
-            credentials: {
-              accessKeyId: env('AWS_ACCESS_KEY_ID'),
-              secretAccessKey: env('AWS_ACCESS_SECRET'),
-            },
-            region: env('AWS_REGION'),
-            params: {
-              ACL: env('AWS_ACL', 'public-read'),
-              signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
-              Bucket: env('AWS_BUCKET'),
-            },
-          },
-        },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
+          key: process.env.DO_SPACE_ACCESS_KEY,
+          secret: process.env.DO_SPACE_SECRET_KEY,
+          endpoint: process.env.DO_SPACE_ENDPOINT,
+          space: process.env.DO_SPACE_BUCKET,
+          directory: "media", // optional
         },
       },
     },
-    // ...
   });
